5a15af2c36063562ab254120fe98647b14d3b040
viator <viator@via-net.org>
2012-09-30 16:40:21 +0400

    map3d - perlin

--- dev/modules/world/map3d.py
+++ dev/modules/world/map3d.py
@@ -4,11 +4,65 @@
 3D map with perlin algorithm
 """
 
-class Map3d(dict):
-    def __init__(self, *args, **params):
-        dict.__init__(self, *args, **params)
+import random
+from pandac.PandaModules import PerlinNoise2
+from PIL import Image
 
-    def get_from_2d(self, map2d, seed):
+class Map3d(dict):
+    """Class for map with heights
+
+       Keys: (X, Y)
+       Values = Heights
+    """
+    def __init__(self, map2d, seed, size, *args, **params):
+        dict.__init__(self, *args, **params)
+        self.map2d = map2d
+        self.seed = seed
+        self.perlin = PerlinNoise2(sx = size, sy = size, seed = seed)
+        self.perlin.setScale(20)
+
+    def get_height_place(self, x, y, level):
+        """return height for x, y with level
+
+        x .. level * x, y ..level *y
+        """
+        heights = 0.0
+        count = 0.0
+        for x in xrange(x, x*level):
+            for y in xrange(y, y*level):
+                heights += self[x, y]
+                count += 1.0
+        height = int(round(heights/count))
+
+    def create_image(self, x, y, size, level, filename):
+        """Generate image with heights
+        """
+        image1 = Image.new("RGB", (size, size), (0, 0, 0, 0))
+        for x in xrange(x, size):
+            for y in xrange(y, size):
+                pass
+
+    def __getitem__(self, item):
+        """If item not in dict, when perlin generate and return
+        """
+        if item in self:
+            return dict.__getitem__(self, item)
+        else:
+            # generate perlin height
+            x, y = item
+            self[x, y] = int(self.perlin(x, y) * 10000)
+            return self[x, y].get( (x, y) )
+
+
+if __name__ == "__main__":
+    maps = __import__('map2d')
+    gen = maps.Map_generator_2D(iters = 3)
+    for i, desc in gen.start():
         pass
+
+    print gen.maps.get_ascii(i)
+    map2d = gen.end_map
+
+
 # vi: ft=python:tw=0:ts=4
 
--- dev/tests/perlin.py
+++ dev/tests/perlin.py
@@ -16,66 +16,114 @@ from PIL import Image
 #map2d = gen.end_map
 
 seed = 123
-size = 256
 
-perlin = PerlinNoise2(size, size)
-# level 1
-perlin.setScale(20)
+def test1():
+    size = 256
 
-#start_x = random.randint(0, size-1024)
-#start_y = random.randint(0, size-768)
-start_x = 0
-start_y = 0
+    perlin = PerlinNoise2(size, size)
+    perlin.setScale(20)
 
-image1 = Image.new("RGB", (size, size), (0, 0, 0, 0))
-image2 = Image.new("RGB", (size, size), (0, 0, 0, 0))
-image3 = Image.new("RGB", (size, size), (0, 0, 0, 0))
-image4 = Image.new("RGB", (size, size), (0, 0, 0, 0))
-main_image = Image.new("RGB", (size*2, size*2), (0, 0, 0, 0))
-cool_image = Image.new("RGB", (size*8, size*8), (0, 0, 0, 0))
+    start_x = 0
+    start_y = 0
 
-perlin_heights = {}
-for x in xrange(0, size):
-    for y in xrange(0, size):
-        color = int(round(perlin(x, y)*100)+100)
-        perlin_heights[(x, y)] = color
+    image1 = Image.new("RGB", (size, size), (0, 0, 0, 0))
+    image2 = Image.new("RGB", (size, size), (0, 0, 0, 0))
+    image3 = Image.new("RGB", (size, size), (0, 0, 0, 0))
+    image4 = Image.new("RGB", (size, size), (0, 0, 0, 0))
+    main_image = Image.new("RGB", (size*2, size*2), (0, 0, 0, 0))
+    cool_image = Image.new("RGB", (size*8, size*8), (0, 0, 0, 0))
 
-for x in xrange(0, size):
-    for y in xrange(0, size):
-        color = perlin_heights[(x, y)]
-        #print x+(i*size), x+(j*size)
-        image1.putpixel((x, y), (0 + color, 0 + color, 0+color))
+    perlin_heights = {}
+    for x in xrange(0, size):
+        for y in xrange(0, size):
+            color = int(round(perlin(x, y)*100)+100)
+            perlin_heights[(x, y)] = color
 
-image2 = image1.copy()
-image3 = image1.copy()
-image4 = image1.copy()
+    for x in xrange(0, size):
+        for y in xrange(0, size):
+            color = perlin_heights[(x, y)]
+            #print x+(i*size), x+(j*size)
+            image1.putpixel((x, y), (0 + color, 0 + color, 0+color))
 
-image2 = image2.transpose(Image.FLIP_LEFT_RIGHT)
-image3 = image3.transpose(Image.FLIP_TOP_BOTTOM)
-image4 = image3.transpose(Image.FLIP_LEFT_RIGHT)
+    image2 = image1.copy()
+    image3 = image1.copy()
+    image4 = image1.copy()
 
-for x in xrange(size):
-    for y in xrange(size):
-        main_image.putpixel((x, y), image1.getpixel((x, y)))
+    image2 = image2.transpose(Image.FLIP_LEFT_RIGHT)
+    image3 = image3.transpose(Image.FLIP_TOP_BOTTOM)
+    image4 = image3.transpose(Image.FLIP_LEFT_RIGHT)
 
-for x in xrange(size, size+size):
-    for y in xrange(size):
-        main_image.putpixel((x, y), image2.getpixel((x-size, y)))
+    for x in xrange(size):
+        for y in xrange(size):
+            main_image.putpixel((x, y), image1.getpixel((x, y)))
 
-for x in xrange(size):
-    for y in xrange(size, size+size):
-        main_image.putpixel((x, y), image3.getpixel((x, y-size)))
+    for x in xrange(size, size+size):
+        for y in xrange(size):
+            main_image.putpixel((x, y), image2.getpixel((x-size, y)))
 
-for x in xrange(size, size+size):
-    for y in xrange(size, size+size):
-        main_image.putpixel((x, y), image4.getpixel((x-size, y-size)))
+    for x in xrange(size):
+        for y in xrange(size, size+size):
+            main_image.putpixel((x, y), image3.getpixel((x, y-size)))
 
-for x in xrange(0, size*8, size*2):
-    for y in xrange(0, size*8, size*2):
-        cool_image.paste(main_image, (x, y))
+    for x in xrange(size, size+size):
+        for y in xrange(size, size+size):
+            main_image.putpixel((x, y), image4.getpixel((x-size, y-size)))
+
+    for x in xrange(0, size*8, size*2):
+        for y in xrange(0, size*8, size*2):
+            cool_image.paste(main_image, (x, y))
 
 
-cool_image.save('heightmap.png', 'PNG')
+    cool_image.save('tiles.png', 'PNG')
+
+def test2():
+    size = 64
+
+    perlin = PerlinNoise2(size, size, size, seed)
+
+    image = Image.new("RGB", (size*6, size), (0, 0, 0, 0))
+    perlin.setScale(20)
+    for x in xrange(0, size):
+        for y in xrange(0, size):
+            color = int(round(perlin(x, y)*100)+100)
+            image.putpixel((x, y), (0 + color, 0 + color, 0+color))
+
+    perlin.setScale(15)
+    for x in xrange(0, size):
+        for y in xrange(0, size):
+            color = int(round(perlin(x, y)*100)+100)
+            image.putpixel((x+size, y), (0 + color, 0 + color, 0+color))
+
+    perlin.setScale(10)
+    for x in xrange(0, size):
+        for y in xrange(0, size):
+            color = int(round(perlin(x, y)*100)+100)
+            image.putpixel((x+(size*2), y), (0 + color, 0 + color, 0+color))
+
+
+    perlin.setScale(7)
+    for x in xrange(0, size):
+        for y in xrange(0, size):
+            color = int(round(perlin(x, y)*100)+100)
+            image.putpixel((x+(size*3), y), (0 + color, 0 + color, 0+color))
+
+    perlin.setScale(5)
+    for x in xrange(0, size):
+        for y in xrange(0, size):
+            color = int(round(perlin(x, y)*100)+100)
+            image.putpixel((x+(size*4), y), (0 + color, 0 + color, 0+color))
+
+    perlin.setScale(2)
+    for x in xrange(0, size):
+        for y in xrange(0, size):
+            color = int(round(perlin(x, y)*100)+100)
+            image.putpixel((x+(size*5), y), (0 + color, 0 + color, 0+color))
+
+    image.save('octaves.png', 'PNG')
+
+test1()
+test2()
+# get x = 3, y = 5  (+1 round)
 # get x = 3, y = 5  (+1 round)
 
 # vi: ft=python:tw=0:ts=4
